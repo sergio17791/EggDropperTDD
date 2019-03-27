7f9a2481b42310ee812f89b2f28d1edc8a878b2c
Sergio Muñoz Buitrón <sergio.m.17791@gmail.com>
2019-03-27 21:31:48 +0100

    Add class Skyscraper

---
+++ src/com/eggdropper/Skyscraper.java
@@ -0,0 +1,13 @@
+package com.eggdropper;
+
+/**
+ * 
+ * @author Sergio Muñoz
+ * This class defines the skyscraper for the egg drop tests
+ */
+public class Skyscraper {
+
+	public Skyscraper() {
+		
+	}
+}
